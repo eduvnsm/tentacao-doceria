296a75833dff8fdfa289340d72837dfb03b674d9
Eduardo V. Micami <eduvnsm@gmail.com>
2024-03-02 18:55:11 -0300

    Criado componente Layout

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
 
 const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<span>HOME</span>} />
+            <Route path="/" element={<Layout>HOME</Layout>} />
             <Route path="/clientes" element={<span>CLIENTES</span>} />
             <Route path="/*" element={<Navigate to="/" />} />
         </Routes>
