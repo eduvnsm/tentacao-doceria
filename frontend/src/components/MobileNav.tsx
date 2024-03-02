f528167db4394f06030337e17e6855b51b41f6d7
Eduardo V. Micami <eduvnsm@gmail.com>
2024-03-02 19:18:28 -0300

    Criado componente MobileNav

---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,25 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from './ui/sheet';
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+        <SheetTrigger>
+            <Menu className="text-orange-500" />
+        </SheetTrigger>
+        <SheetContent>
+            <SheetTitle>
+                <span>Bem vindo à Tentação Doceria</span>
+            </SheetTitle>
+            <Separator />
+            <SheetDescription className="flex">
+                <Button className="flex-1 font-bold bg-orange-500">Entrar</Button>
+            </SheetDescription>
+        </SheetContent>
+    </Sheet>
+  )
+}
+
+export default MobileNav;
