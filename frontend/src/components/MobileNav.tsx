4abbd75a3913407fd960c540fdcee6e17c3f84b1
Eduardo V. Micami <eduvns.dev@gmail.com>
2024-03-03 19:15:17 -0300

    Criado componente MobileNavLinks

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,25 +1,45 @@
-import { Menu } from "lucide-react";
+import { Menu, CircleUserRound } from 'lucide-react';
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from './ui/sheet';
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from './MobileNavLinks';
 
 const MobileNav = () => {
-  return (
-    <Sheet>
-        <SheetTrigger>
-            <Menu className="text-red-600" />
-        </SheetTrigger>
-        <SheetContent className="space-y-3">
-            <SheetTitle>
-                <span>Faça o Login</span>
-            </SheetTitle>
-            <Separator />
-            <SheetDescription className="flex">
-                <Button className="flex-1 font-bold bg-red-600">Entrar</Button>
-            </SheetDescription>
-        </SheetContent>
-    </Sheet>
-  )
+
+    const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Menu className="text-red-600" />
+            </SheetTrigger>
+            <SheetContent className="space-y-3">
+                <SheetTitle>
+                    {isAuthenticated ? (
+                        <span className="flex items-center font-bold gap-2">
+                            <CircleUserRound className="text-red-600" />
+                            {user?.email}
+                        </span>
+                    ) : (
+                        <span>Faça o Login</span>
+                    )}
+                </SheetTitle>
+                <Separator />
+                <SheetDescription className="flex flex-col gap-4">
+                    {isAuthenticated ? (
+                        <MobileNavLinks />
+                    ) : (
+                        <Button
+                            onClick={async () => await loginWithRedirect()}
+                            className="flex-1 font-bold bg-red-600"
+                        >
+                            Entrar
+                        </Button>
+                    )}
+                </SheetDescription>
+            </SheetContent>
+        </Sheet>
+    );
 }
 
 export default MobileNav;
