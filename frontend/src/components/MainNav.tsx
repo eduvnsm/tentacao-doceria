1596180bb090d932f2b79cba3e11feaf232fa02d
Eduardo V. Micami <eduvns.dev@gmail.com>
2024-03-03 18:10:09 -0300

    Criado componente de autenticação Auth0

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,10 +1,14 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+
+  const { loginWithRedirect } = useAuth0();
   return (
     <Button
         variant="ghost"
         className="font-bold hover:text-red-600 hover:bg-white"
+        onClick={async () => await loginWithRedirect()}
     >
         Entrar
     </Button>
